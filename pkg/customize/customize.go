5555ed6c3811ef6c7dfff0c9a3cdea6fc798dd92
Caleb Lindgren <calebmlindgren@gmail.com>
2026-02-11 13:06:30 -0500

    Add customization package

---
+++ pkg/customize/customize.go
@@ -0,0 +1,11 @@
+// Package customize provides custom configuration
+package customize
+
+import (
+	"github.com/ThomasHabets/cmdg/pkg/input"
+)
+
+// Customizable interaction keys
+const (
+	Leave = input.Enter
+)
